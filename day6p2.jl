ef757f70f254f4c928536102ed686e00ad888ba7
Lukas Mericle <lukas.mericle@gmail.com>
2020-12-22 19:25:03 -0800

    day 6 pt 2

---
+++ day6p2.jl
@@ -0,0 +1,15 @@
+fname = "day6input.txt"
+rows = readlines(fname)
+form = String[]
+n = 0
+for row=rows
+    global form, n
+    if (row == "")
+        n += length(intersect(Set.(form)...))
+        form = String[]
+    else
+        push!(form, row)
+    end
+end
+(length(form) > 0) && (n += length(intersect(Set.(form)...)))
+println(n)

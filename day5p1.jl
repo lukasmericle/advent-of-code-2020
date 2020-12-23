ce4f14c3be65d38ce84f921eceaa11f46254911d
Lukas Mericle <lukas.mericle@gmail.com>
2020-12-22 19:08:01 -0800

    day 5 pt 1

---
+++ day5p1.jl
@@ -0,0 +1,13 @@
+seat_id(row, col) = row * 8 + col
+function find_row(part::AbstractString; level::Int=0)
+    (part == "") && return 0
+    2^level * convert(Int, last(part) == 'B') + find_row(part[1:end-1], level=level+1)
+end
+function find_col(part::AbstractString; level::Int=0)
+    (part == "") && return 0
+    2^level * convert(Int, last(part) == 'R') + find_col(part[1:end-1], level=level+1)
+end
+find_seat(part::String) = find_row(part[1:7]), find_col(part[8:end])
+fname = "day5input.txt"
+rows = readlines(fname)
+println(max(map(row -> seat_id(find_seat(row)...), rows)...))
